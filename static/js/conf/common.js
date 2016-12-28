10ba61ce04ccddcc2df96d53699e1930a073a957
carer <454875315@qq.com>
2016-12-28 17:41:27 +0800

    d

--- static/js/conf/common.js
+++ static/js/conf/common.js
@@ -3,11 +3,16 @@ define(function(require, exports, module) {
 
 	var $ = require('jquery');
 
+	function setTime(date) {
+		var date = new Date(date);
+		return date.toLocaleDateString();
+	}
+
 	$.get('/list/hot', function(data) {
 		var data = data.data;
 		var html = '';
 		for (var i = 0; i < data.length; i++) {
-			html+='<li><img src="'+data[i].img+'"/><div class="text"><a href="/item?id='+ data[i].id +'">'+ data[i].title +'</a><span>'+data[i].dataTime+'</span></div></li>'
+			html += '<li><img src="' + data[i].img + '"/><div class="text"><a href="/item?id=' + data[i].id + '">' + data[i].title + '</a><span>' + setTime(data[i].dataTime) + '</span></div></li>'
 		}
 		$('#hotList').html(html);
 	})
